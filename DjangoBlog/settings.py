20ccd33ce1983c215f2d3773a276585ee9e21aaa
Patterbear <benjamin.mcgregor2002@gmail.com>
2022-08-25 12:02:56 +0100

    login and logout system

--- DjangoBlog/settings.py
+++ DjangoBlog/settings.py
@@ -129,3 +129,9 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Redirects user to home page upon login
+LOGIN_REDIRECT_URL = 'blog-home'
+
+# Sets login URL
+LOGIN_URL = 'login'
